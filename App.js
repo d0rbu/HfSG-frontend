1705b97b1bca4e71d908d9da8b092dfc446eb935
d0rb <hacperu2010@gmail.com>
2022-09-24 16:12:26 -0500

    Added google authentication !

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { useState, useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
+import { Button } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    responseType: "id_token",
+    expoClientId: '220207363229-cje1vv3oe4njcdlsmjhi9gpeumm7g1mj.apps.googleusercontent.com',
+    iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    androidClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    webClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+  }, );
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      setUser(response);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HfSG App</Text>
+      <Text>{response}</Text>
       <StatusBar style="auto" />
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();}
+        }
+      />
     </View>
   );
 }
