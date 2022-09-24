e9539624d84d857b0a80f643bbdb99cb7a0721b5
d0rb <hacperu2010@gmail.com>
2022-09-24 16:59:22 -0500

    Displays unique user id on login

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as WebBrowser from 'expo-web-browser';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Google from 'expo-auth-session/providers/google';
 import { Button } from 'react-native';
+import { Buffer } from "buffer";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -20,14 +21,26 @@ export default function App() {
 
   useEffect(() => {
     if (response?.type === 'success') {
-      setUser(response);
+      const id_token = response.params.id_token
+
+      const jwt_parts = id_token
+        .split(".")
+        .map((jwt_part) =>
+          Buffer.from(
+            jwt_part.replace(/-/g, "+").replace(/_/g, "/"),
+            "base64"
+          ).toString()
+        );
+      const user = JSON.parse(jwt_parts[1]); // get user object after jwt decoded
+
+      setUser(user);
     }
   }, [response]);
 
   return (
     <View style={styles.container}>
       <Text>HfSG App</Text>
-      <Text>{response}</Text>
+      <Text>{user?.sub}</Text>
       <StatusBar style="auto" />
       <Button
         disabled={!request}
